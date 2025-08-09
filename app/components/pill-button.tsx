0368461fe2a56e59bc473ff17e977402d3bd520f
ivstefano <ivo.stefanov90@gmail.com>
2025-08-09 21:30:00 +0300

    Fixed error reference

--- app/components/pill-button.tsx
+++ app/components/pill-button.tsx
@@ -13,7 +13,6 @@ export const PillButton = React.forwardRef<
 >(({ children, icon: Icon, className, ...props }, ref) => {
   return (
     <ReflectiveButton
-      ref={ref}
       variant="outline"
       className={`h-8 px-3 rounded-full bg-gray-200 dark:bg-[#333333] hover:bg-gray-300 dark:hover:bg-[#333333] text-gray-700 dark:text-gray-200 hover:text-gray-800 dark:hover:text-gray-200 shadow-sm border border-gray-300 dark:border-gray-700 text-[13px] font-medium whitespace-nowrap inline-flex items-center ${className}`}
       {...props}
